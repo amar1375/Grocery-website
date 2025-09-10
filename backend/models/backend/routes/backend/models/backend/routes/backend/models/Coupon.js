3d3a34b2dfc1372603c72457c681e603f89296ee
amar1375 <142719124+amar1375@users.noreply.github.com>
2025-09-10 12:07:26 +0530

    Create Coupon.js

---
+++ backend/models/backend/routes/backend/models/backend/routes/backend/models/Coupon.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const couponSchema = new mongoose.Schema(
+  {
+    code: {
+      type: String,
+      required: true,
+      unique: true,
+      uppercase: true,
+      trim: true,
+    },
+    discountPercentage: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 100,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    }
+  },
+  { timestamps: true }
+);
+
+const Coupon = mongoose.model("Coupon", couponSchema);
+export default Coupon;
